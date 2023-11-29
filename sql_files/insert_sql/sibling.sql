8557464870e1b01162f09720e5c7de3388e9077b
RazanYakoub01 <131905291+RazanYakoub01@users.noreply.github.com>
2023-11-29 13:30:57 +0100

    more siblings

--- sql_files/insert_sql/sibling.sql
+++ sql_files/insert_sql/sibling.sql
@@ -17,3 +17,27 @@ VALUES
   (13,33),
   (14,34),
   (15,35);
+INSERT INTO sibling_student (student_id,sibling_id)
+VALUES
+  (3,80),
+  (8,79),
+  (13,78),
+  (18,77),
+  (23,76),
+  (28,75),
+  (33,74),
+  (38,73),
+  (43,72),
+  (48,71);
+INSERT INTO sibling_student (student_id,sibling_id)
+VALUES
+  (1,40),
+  (4,42),
+  (7,44),
+  (10,46),
+  (13,48),
+  (16,50),
+  (19,52),
+  (22,54),
+  (25,56),
+  (28,58);
