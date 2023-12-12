b673aa961b7f1ef1e7de3f1cc08bb1c6390a34e3
RazanYakoub01 <131905291+RazanYakoub01@users.noreply.github.com>
2023-12-12 19:25:27 +0100

    main method

--- Seminar_4/src/main/java/se/kth/iv1351/soundschooldbc/startup/Main.java
+++ Seminar_4/src/main/java/se/kth/iv1351/soundschooldbc/startup/Main.java
@@ -3,6 +3,10 @@
  */
 package main.java.se.kth.iv1351.soundschooldbc.startup;
 
+import main.java.se.kth.iv1351.soundschooldbc.controller.Controller;
+import main.java.se.kth.iv1351.soundschooldbc.integration.SchoolDBException;
+import main.java.se.kth.iv1351.soundschooldbc.view.BlockingInterpreter;
+
 /**
  * @author razan
  *
@@ -10,6 +14,11 @@ package main.java.se.kth.iv1351.soundschooldbc.startup;
 
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            new BlockingInterpreter(new Controller()).handleCmds();
+        } catch (SchoolDBException bdbe) {
+            System.out.println("Could not connect to School db.");
+            bdbe.printStackTrace();
+        }
     }
 }
--- Seminar_4/src/main/java/se/kth/iv1351/soundschooldbc/view/BlockingInterpreter.java
+++ Seminar_4/src/main/java/se/kth/iv1351/soundschooldbc/view/BlockingInterpreter.java
@@ -1,5 +1,13 @@
 package main.java.se.kth.iv1351.soundschooldbc.view;
 
+import main.java.se.kth.iv1351.soundschooldbc.controller.Controller;
+
 public class BlockingInterpreter {
+
+    private Controller ctrl;
+
+    public BlockingInterpreter(Controller ctrl) {
+        this.ctrl = ctrl;
+    }
 
 }
